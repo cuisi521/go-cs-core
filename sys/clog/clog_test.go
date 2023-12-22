85f270cd0010c227d6647fcda64a8791df9df3d0
cuisi521 <14163829@qq.com>
2023-12-22 09:30:55 +0800

    fix:日志组件更新

--- sys/clog/clog_test.go
+++ sys/clog/clog_test.go
@@ -17,5 +17,6 @@ func TestLog(t *testing.T) {
 	// Error("This is an informational message")
 	// }
 	Infof("%s 111", fmt.Sprintf("\033[%dm %d \033[0m", 200, 200))
+
 	// WidthFields(logrus.Fields{"animal": "walres"}).Info("A group of walrus emerges from the ocean")
 }
